e601bcabda17cf363760bd981c798ab0144db5ed
Ryan358 <69889842+Ryan358@users.noreply.github.com>
2022-06-06 19:25:49 -0700

    Added documentation and updated README.md

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""This code MUST be named main.py. Otherwise, it will not run by default on the Pico.
+This code is meant to be flashed using Thonny or your preferred editor
+onto the Pico/microcontroller to get the position of the servos.
+It does not actually actively read the positions. Instead, it defines the get_pos function,
+and then waits. No code is run until the setup/damping code sends the get_pos function over the REPL
+through serial communication. However, get_pos can be run independently to check
+that the Pico is working and reading the position properly."""
+
 from machine import Pin, ADC
 from time import sleep
 
@@ -6,16 +14,20 @@ servo1pos = ADC(26)
 servo2pos = ADC(27)
 
 
-def getPos():
-    pos = [300, 400]
-    # pos = [servo1pos.read_u16(), servo2pos.read_u16()]
+def get_pos():
+    """This function gets the position of the servos by reading the two analog inputs,
+    and returns the position as a two-entry list. It also prints the position for serial communication."""
+    pos = [servo1pos.read_u16(), servo2pos.read_u16()]  # positions returned in integer form.
+    # This print line is necessary for the serial communication.
     print(pos)
+    # The return line is used for running the function during testing. Suggested to keep even after deployment.
     return pos
 
 
 def main():
+    """This code is just a simple way to check the function's operation."""
     print("Reading Position...")
-    pos = getPos()
+    pos = get_pos()
     sleep(0.5)
     print(f"Servo 1 is at {pos[0]} and Servo 2 is at {pos[1]}")
 
--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+"""This code contains all the setup information for the servos and accelerometer. It also
+contains the calibration function which is called by the main damping code. This module is mainly imported,
+but can be run independently to calibrate the servos whenever necessary. """
+
 # from gpiozero import Device, Servo
 # This is the default import statement used for deployment. The only essential import is Servo.
 
@@ -10,13 +14,15 @@
 
 
 # The following imports and pin factory assignments are used to testing on a PC with no GPIO Pins.
-# #For deployment, use the above import/pin factory.
+# For deployment, use the above import/pin factory.
 from gpiozero import Device, Servo
 from gpiozero.pins.mock import MockFactory, MockPWMPin
 
 from time import sleep
 from sender import Sender
 import json
+import board
+import adafruit_mma8451
 
 # This line forces the code to use fake pins for testing. Remove when testing on an actual circuit.
 Device.pin_factory = MockFactory(pin_class=MockPWMPin)
@@ -24,33 +30,43 @@ Device.pin_factory = MockFactory(pin_class=MockPWMPin)
 # For more info/troubleshooting on the pin factories and servo class,
 # check the docs at https://gpiozero.readthedocs.io/en/stable/index.html
 
+# declare servo pins, and initialize the accelerometer. This code always runs when imported.
 servo1 = Servo(9)
 servo2 = Servo(10)
+i2c = board.I2C()
+sensor = adafruit_mma8451.MMA8451(i2c)
 
 
 def calibrate():
-    """Calibrates the servo and provides voltage values at max and min positions"""
-    port = 'COM8'  # replace this with the correct port
+    """Calibrates the servo and provides voltage values at max and min positions. It does this by
+    sending the get_pos command to the Pico over serial communication, and then stores these in a
+    variable called cur_pos. This is then """
+    port = 'COM8'  # replace this with the correct port. This initializes serial communication.
     s = Sender(port)
-    curPos = []
+    # cur_pos will be a list of lists. The first entry is the minimum positions, and the second is the maximum.
+    cur_pos = []
     servo1.min()
     servo2.min()
     sleep(2)  # allow time for servos to settle
-    s.send('getPos()')
-    curPos.append(json.loads(s.receive()))
-    minPos = curPos[0]
-    sleep(0.5)
+    s.send('get_pos')
+    cur_pos.append(json.loads(s.receive()))
+    min_pos = cur_pos[0]
+    sleep(1)
     servo1.max()
     servo2.max()
     sleep(2)  # allow time for servos to settle
-    s.send('getPos()')
+    s.send('get_pos')
     s.receive()
-    curPos.append(json.loads(s.receive()))
-    maxPos = curPos[1]
+    cur_pos.append(json.loads(s.receive()))
+    max_pos = cur_pos[1]
     s.close()
+
+    # this saves the current positions (min and max) as a json file that is used as a reference.
     with open('bounds.json', 'w') as f:
-        json.dump(curPos, f)
-    return minPos, maxPos
+        json.dump(cur_pos, f)
+
+    # The return statement exists because the calibration file will be called by the main code if no json file exists.
+    return min_pos, max_pos
 
 
 def main():
